d3271b182664b7b9fbdb52d69c3e1992e867290d
Santosh Padhiali <santoshpadhiali007@gmail.com>
2025-07-13 14:55:46 +0530

    project done

--- BACKEND/src/routes/message.routs.js
+++ BACKEND/src/routes/message.routs.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
@@ -11,10 +12,10 @@ const router = express.Router();
 // GET all users except logged-in user
 router.get("/user", protectRoute, getUsersForSidebar);
 
+// GET messages with a specific user
+router.get("/:id", protectRoute, getMessages);
+
 // POST send a message to a user
 router.post("/send/:id", protectRoute, sendMessages);
-
-// GET messages with a specific user
-router.get("/chat/:id", protectRoute, getMessages); // ✅ safer route name
 
 export default router;
--- FRONTANT/vite-project/src/lib/axios.js
+++ FRONTANT/vite-project/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstant = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
+
